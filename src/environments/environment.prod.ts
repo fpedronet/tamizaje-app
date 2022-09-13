dcc86e7907b6854ef031934173e3393b7dd62063
fpedronet <36646447+fpedronet@users.noreply.github.com>
2022-09-13 14:44:57 -0500

    merge

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,11 +4,11 @@ export const environment = {
   // UrlApi:'https://service.poclab.pe/silac/api',  
   // UrlImage:'assets/',
 
-  // UrlApi:'https://sitedev.poclab.pe/maternidaddev/api',  
-  // UrlImage:'assets/',
+  UrlApi:'https://sitedev.poclab.pe/maternidaddev/api',  
+  UrlImage:'assets/',
 
-         UrlApi:'http://localhost:48394/api',
-         UrlImage:'../../../../assets/',
+        //  UrlApi:'http://localhost:48394/api',
+        //  UrlImage:'../../../../assets/',
 
   TOKEN_AUTH_USERNAME: 'mitomediapp',
   TOKEN_AUTH_PASSWORD: 'mito89codex',
